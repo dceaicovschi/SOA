86c72e3c88072fa999f96945e5b879dbd583b126
dceaicovschi <dumitru.ceaicovschi@endava.com>
2021-01-12 14:55:30 +0200

    change text

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,6 @@ import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hi nca");
+        System.out.println("hi nca new text");
     }
 }
